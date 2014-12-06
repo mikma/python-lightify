6c3d8f31a6b09e1642e37b1e8f2bdef5d91efc36
Mikael Magnusson <mikma@users.sourceforge.net>
2014-12-06 23:20:27 +0100

    Move test program out of lightify.py

---
+++ test.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+#
+# Copyright 2014 Mikael Magnusson
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import lightify
+import sys
+
+def main(argv):
+
+    conn = lightify.Lightify(argv[1])
+    groups = conn.group_list()
+
+    for group in groups.iterkeys():
+        lights = conn.group_info(group)
+        print "Lights: %s" % lights
+
+#        for light in lights:
+#            conn.read_light_status(light)
+
+    #conn.read_all_light_status(1)
+
+    sys.exit(0)
+
+#seq = seq + 1
+#data = build_command(seq, 0x35, 2, "") #, struct.pack("<B", 0))
+#print 'sending "%s"' % binascii.hexlify(data)
+##, struct.pack(""))
+#sock.sendall(data)
+#recv(sock)
+#sock.close()
+#sys.exit(0)
+
+    seq = seq + 1
+#data = onoff(seq, 2, 0)
+#data = build_temp(seq, 2, 2700)
+    data = build_luminance(seq, 2, 85, 2)
+#data = colour(seq, 2, 0, 0, 255, 1)
+    print 'sending "%s"' % binascii.hexlify(data)
+    sock.sendall(data)
+    recv(sock)
+
+    time.sleep(2)
+
+    seq = seq + 1
+#data = onoff(seq, 2, 0)
+#data = build_temp(seq, 2, 2700)
+    data = build_luminance(seq, 2, 65, 2)
+#data = colour(seq, 2, 255, 0, 0, 10)
+    print 'sending "%s"' % binascii.hexlify(data)
+    sock.sendall(data)
+    recv(sock)
+
+    time.sleep(2)
+
+    seq = seq + 1
+#data = onoff(seq, 2, 1)
+#data = build_temp(seq, 2, 6500)
+    data = build_luminance(seq, 2, lum, 10)
+#data = colour(seq, 2, 255, 255, 255, 1)
+    print 'sending "%s"' % binascii.hexlify(data)
+    sock.sendall(data)
+    recv(sock)
+
+
+
+    sock.close()
+    sys.exit(0)
+
+
+main(sys.argv)
